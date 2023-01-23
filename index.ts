8cc9e838028a8f126fda8779f380c059884beb4a
Aliaksei539 <aholonevec@mail.com>
2023-01-23 23:46:29 +0300

    create

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {renderPageGarage} from './modules/render-page';
+renderPageGarage()
+
+
+
+const carOne = document.getElementById('1');
+const btnStart1 = document.getElementById('btnStart1');
+btnStart1?.addEventListener('click', ()=>{
+    console.log('Hello')
+} )
